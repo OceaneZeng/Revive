42cbebb4515441cf4ef49e94a3c5c6ff195b980c
OceaneZeng <2364234422@qq.com>
2025-04-22 20:41:19 +0800

    First Source

---
+++ Source/Revive/Core/public/ReviveGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ReviveGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AReviveGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AReviveGameMode();
+};
+
+
+
